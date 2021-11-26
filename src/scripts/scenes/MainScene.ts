aca36738a89e9b79028e3749ae3ba8bb1fd52811
Golen <golen87@hotmail.com>
2021-11-26 20:37:23 +0100

    Added input test.

--- src/scripts/scenes/MainScene.ts
+++ src/scripts/scenes/MainScene.ts
@@ -3,6 +3,8 @@ import { RoundRectangle } from "../components/RoundRectangle";
 
 
 export class MainScene extends BaseScene {
+	myImage: Phaser.GameObjects.Image;
+	keys: any;
 
 	constructor() {
 		super({key: "MainScene"});
@@ -11,9 +13,29 @@ export class MainScene extends BaseScene {
 	create(): void {
 		// this.fade(false, 200, 0x000000);
 
-		this.add.image(this.CX, this.CY, "pika");
+		// Adding an image
+		// https://rexrainbow.github.io/phaser3-rex-notes/docs/site/image/
+		this.myImage = this.add.image(500, 500, "pika");
+
+
+		// Input
+		// https://rexrainbow.github.io/phaser3-rex-notes/docs/site/keyboardevents/
+		this.keys = this.input.keyboard.addKeys({
+			up: 'up',
+			down: 'down',
+			left: 'left',
+			right: 'right'
+		});
 	}
 
 	update(time: number, delta: number) {
+		if (this.keys.left.isDown)
+			this.myImage.x -= 5;
+		if (this.keys.right.isDown)
+			this.myImage.x += 5;
+		if (this.keys.down.isDown)
+			this.myImage.y += 5;
+		if (this.keys.up.isDown)
+			this.myImage.y -= 5;
 	}
 }
