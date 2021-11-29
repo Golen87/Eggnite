ae2617d1b757fb97aa728d3fcfc4d6ca31f5d6ec
Golen <golen87@hotmail.com>
2021-11-29 01:29:41 +0100

    Add title card. Add intro cutscene.

--- src/scripts/scenes/OverworldScene.ts
+++ src/scripts/scenes/OverworldScene.ts
@@ -1,5 +1,6 @@
 import { BaseScene } from "./BaseScene";
 import { RoundRectangle } from "../components/RoundRectangle";
+import BendWaves2 from "../pipelines/BendWavesPostFX2";
 
 
 export interface Point {
@@ -14,13 +15,17 @@ export class OverworldScene extends BaseScene {
 
 	clouds1: Phaser.GameObjects.Image;
 	clouds2: Phaser.GameObjects.Image;
+	volcano: Phaser.GameObjects.Image;
 	player: Phaser.GameObjects.Image;
-	flags: Phaser.GameObjects.Image[];
 	points: any;
 	curves: any;
 	graphics: Phaser.GameObjects.Graphics;
 	victory: Phaser.GameObjects.Text;
-	timer: number;
+	title: Phaser.GameObjects.Text;
+	desc: Phaser.GameObjects.Text;
+
+	introPlaying: boolean;
+	myTweens: Phaser.Tweens.Tween[];
 
 	constructor() {
 		super({key: "OverworldScene"});
@@ -31,7 +36,11 @@ export class OverworldScene extends BaseScene {
 	}
 
 	create(): void {
-		this.fade(false, (this.level == 0) ? 2000 : 500, 0x000000);
+		this.fade(false, (this.level == 0) ? 2000 : 500, 0xFFFFFF);
+		this.cameras.main.setBackgroundColor(0x7bb7e7);
+
+		this.introPlaying = false;
+		this.myTweens = [];
 
 
 		// Images
@@ -40,10 +49,10 @@ export class OverworldScene extends BaseScene {
 		this.clouds2 = this.add.image(0, 0, "clouds");
 		this.clouds1.setOrigin(0);
 		this.clouds2.setOrigin(0);
-		let volcano = this.add.image(this.CX, this.CY, "volcano");
+		this.volcano = this.add.image(this.CX, this.CY, "volcano");
 		this.fitToScreen(this.clouds1);
 		this.fitToScreen(this.clouds2);
-		this.fitToScreen(volcano);
+		this.fitToScreen(this.volcano);
 
 		this.player = this.add.image(this.CX, this.CY, "mapplayer", 0);
 		this.player.setOrigin(0.5, 0.7);
@@ -54,14 +63,21 @@ export class OverworldScene extends BaseScene {
 		this.graphics.lineStyle(1, 0xEEEEDD, 1.0);
 		this.graphics.fillStyle(0xEEEEDD, 1.0);
 
-		this.flags = [];
+
+		this.title = this.createText(this.CX, 0.43*this.H, 40, this.weights.bold, "#FA4", "EGGNITE").setOrigin(0.5).setStroke("#732", 6);
+		this.title.setPostPipeline(BendWaves2);
+		this.title.setVisible(false);
+
+		this.desc = this.createText(this.CX, 0.65*this.H, 18, this.weights.bold, "#FFF").setOrigin(0.5);
+		this.desc.setText("Tap to play");
+		this.desc.setVisible(false);
 
 
 		let levels = [
-			{ x:2*74, y:2*77 },
-			{ x:2*37, y:2*60 },
-			{ x:2*101, y:2*45 },
-			{ x:2*73, y:2*19 },
+			{ x:2*80, y:2*77 },
+			{ x:2*43, y:2*60 },
+			{ x:2*107, y:2*45 },
+			{ x:2*79, y:2*19 },
 		];
 		this.points = {};
 		this.curves = {};
@@ -119,7 +135,6 @@ export class OverworldScene extends BaseScene {
 		for (let i=0; i<this.level; i++) {
 			// let flag = this.add.image(levels[i].x, levels[i].y, "mapflag")
 			// flag.setOrigin(0.5, 0.8);
-			// this.flags.push(flag);
 
 			this.points[i].forEach((point: any, index: number) => {
 				this.graphics.fillCircle(point.x, point.y, point.radius);
@@ -127,59 +142,24 @@ export class OverworldScene extends BaseScene {
 		}
 
 		// Animate
-		this.addEvent((this.level == 0) ? 2000 : 1000, () => {
-			if (this.level > 3) {
-				this.victory = this.createText(this.CX, this.CY, 60, this.weights.bold, "#F00", "VICTORY").setOrigin(0.5).setStroke("#FF0", 6);
-				return;
-			}
-
-			this.points[this.level].forEach((point: any, index: number) => {
-				this.addEvent(200*index, () => {
-					this.graphics.fillCircle(point.x, point.y, point.radius);
-				})
-			});
-
-			const wait = 200 * this.points[this.level].length;
-
-			let curve = this.curves[this.level];
-			if (curve) {
-
-				// Animate player
-				let tween = this.tweens.add({
-					targets: this,
-					timer: { from: 0, to: 1 },
-					duration: wait,
-					delay: wait,
-				});
-
-				// Move player
-				tween.on('update', (tween, key, target, current, previous) => {
-					let p = curve.getPoint(1-current);
-					this.player.x = p.x;
-					this.player.y = p.y;
-				}, this);
-
-				// Trigger next level
-				tween.on('complete', this.progress, this);
-			}
-			else {
-				// Animate player
-				let tween = this.tweens.add({
-					targets: this.player,
-					y: "-=40",
-					duration: 1000,
-					delay: wait + 500,
-				});
-				tween.on('complete', this.progress, this);
-			}
-		}, this);
+		if (this.level == 0) {
+			this.showIntroAnimation();
+		}
+		else
+		{
+			this.addEvent(1000, this.showStageAnimation, this);
+		}
 
 		// Skip
 		this.input.keyboard.on("keydown-ESC", () => {
+			this.skip();
 			if (this.level < 4) {
 				this.scene.start("MainScene", { level: this.level });
 			}
 		}, this);
+
+		this.input.keyboard.on("keydown-SPACE", this.skip, this);
+		this.input.on('pointerdown', this.skip, this);
 	}
 
 	update(time: number, delta: number) {
@@ -193,14 +173,111 @@ export class OverworldScene extends BaseScene {
 		if (this.victory) {
 			this.victory.setScale(1.0 + 0.1*Math.sin(5*time/1000));
 		}
-		// this.flags.forEach(flag => {
 			// flag.setFrame(anim ? 0 : 1);
 		// });
 	}
 
+	showIntroAnimation() {
+		this.introPlaying = true;
+
+		this.clouds1.y += 80;
+		this.clouds2.y += 80;
+		this.volcano.y += 300;
+
+		this.myTweens.push(this.tweens.add({
+			targets: [this.clouds1, this.clouds2],
+			y: "-=80",
+			ease: 'Cubic.InOut',
+			duration: 5000,
+			delay: 0,
+		}));
+
+		let vtween = this.tweens.add({
+			targets: [this.volcano],
+			y: "-=300",
+			ease: 'Cubic.InOut',
+			duration: 5000,
+			delay: 0,
+		});
+		this.myTweens.push(vtween);
+
+		vtween.on("stop", this.revealTitle, this);
+		vtween.on("complete", this.revealTitle, this);
+	}
+
+	revealTitle() {
+		this.flash(1000, 0xFFFFFF);
+		this.title.setVisible(true);
+		this.desc.setVisible(true);
+		this.introPlaying = false;
+	}
+
+	showStageAnimation() {
+		if (this.level > 3) {
+			this.flash(1000, 0xFFFFFF);
+			this.victory = this.createText(this.CX, this.CY, 60, this.weights.bold, "#F00", "VICTORY").setOrigin(0.5).setStroke("#FF0", 6);
+			return;
+		}
+
+		this.points[this.level].forEach((point: any, index: number) => {
+			this.addEvent(200*index, () => {
+				this.graphics.fillCircle(point.x, point.y, point.radius);
+			})
+		});
+
+		const wait = 200 * this.points[this.level].length;
+
+		let curve = this.curves[this.level];
+		if (curve) {
+
+			// Animate player
+			let tween = this.tweens.add({
+				targets: this,
+				timer: { from: 0, to: 1 },
+				duration: wait,
+				delay: wait,
+			});
+
+			// Move player
+			tween.on('update', (tween, key, target, current, previous) => {
+				let p = curve.getPoint(1-current);
+				this.player.x = p.x;
+				this.player.y = p.y;
+			}, this);
+
+			// Trigger next level
+			tween.on('complete', this.progress, this);
+		}
+		else {
+			// Animate player
+			let tween = this.tweens.add({
+				targets: this.player,
+				y: "-=40",
+				duration: 1000,
+				delay: wait + 500,
+			});
+			tween.on('complete', this.progress, this);
+		}
+	}
+
+	skip() {
+		if (this.introPlaying) {
+			this.myTweens.forEach((tween: Phaser.Tweens.Tween) => {
+				tween.stop(1);
+			});
+		}
+		else if (this.title.visible) {
+			this.title.setVisible(false);
+			this.desc.setVisible(false);
+			this.showStageAnimation();
+		}
+
+		this.cameras.main.fadeEffect.reset();
+	}
+
 	progress() {
-		this.addEvent(1000, () => {
-			this.fade(true, 500, 0x000000);
+		this.addEvent(500, () => {
+			this.fade(true, 1000, 0x000000);
 			this.addEvent(550, () => {
 				this.scene.start("MainScene", { level: this.level });
 			});
